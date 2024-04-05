18802f1bd6ac8a17455cbfca4138e5c60deb2956
Chris Ioannidis <chris.ioannidis23@imperial.ac.uk>
2024-04-05 17:08:44 +0300

    Added logging

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
+
+import logging
 import concurrent.futures
+
 from src.Scraper import Scraper
 from src.Profile import Profile
 from src.Database import Database
+from src.WebUI import WebUI
 
 
 def main():
+    logging.info("Starting Staff Finder")
+    ui = WebUI()
+    logging.info("WebUI created")
     scraper = Scraper()
-    urls = scraper.scrape()
+    logging.info("Scraper created")
     db = Database('profiles.db')
+    logging.info("Database created")
 
-    # Use ThreadPoolExecutor to parallelize profile creation
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        # Map each URL to the Profile constructor, creating profiles in parallel
-        future_to_url = {executor.submit(Profile, url): url for url in urls}
-        for future in concurrent.futures.as_completed(future_to_url):
-            url = future_to_url[future]
-            try:
-                profile = future.result()
-                print(f"Profile for {profile.get_data('name')} created")
-                db.insert_profile(profile)
-            except Exception as exc:
-                print('%r generated an exception: %s' % (url, exc))
+    while True:
+        urls = scraper.scrape()
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            urls = [url for url in urls if not db.profile_exists(url)]
+            future_to_url = {executor.submit(Profile, url): url for url in urls}
+            for future in concurrent.futures.as_completed(future_to_url):
+                url = future_to_url[future]
+                try:
+                    profile = future.result()
+                    if not db.profile_exists(profile.url):
+                        logging.info(f"Profile for {profile.get_data('name')} created")
+                        db.insert_profile(profile)
+                except Exception as exc:
+                    logging.error(f"Profile for {url} failed to create")
+                    logging.error(exc)
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 if __name__ == "__main__":
+    # logging.disable(logging.CRITICAL)
     main()
